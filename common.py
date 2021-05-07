ba6181559e3614f4ce977603626bae83bf39c18d
madisyn <madisyn.maldonado13@gmail.com>
2021-05-06 20:51:12 -0700

    Add files via upload

--- common.py
+++ common.py
@@ -1,1 +1,16 @@
-#common file
+"""
+This file defines cache, session, and translator T object for the app
+These are fixtures that every app needs so probably you will not be editing this file
+"""
+import copy
+import os
+import sys
+import logging
+from py4web import Session, Cache, Translator, Flash, DAL, Field, action
+from py4web.utils.mailer import Mailer
+from py4web.utils.auth import Auth
+from py4web.utils.downloader import downloader
+from py4web.utils.tags import Tags
+from py4web.utils.factories import ActionFactory
+from py4web.utils.form import FormStyleBulma
+from . import settings
--- models.py
+++ models.py
@@ -16,45 +16,50 @@ def get_time():
 
 ### Define your table below
 #
-# db.define_table('thing', Field('name'))
+# db.define_table('thing', Field('name')) 
 #
 ## always commit your models to avoid problems later
 
 db.define_table(
     'location_catalog',
-    Field('Title'),
-    Field('x_coordinate'),
-    Field('y_coordinate'),
-    Field('date_location_found'),
-    Field('overall_rating'),
+    Field('Title', 'text'),
+    Field('x_coordinate', 'integer'),
+    Field('y_coordinate', 'integer'),
+    Field('date_location_found', 'datetime'),
+    Field('overall_rating', 'integer'),
 )
 
 
 db.define_table(
     'weep_reviews',
     Field('review_id', 'reference location_catalog', ondelete="CASCADE"),
-    Field('noise_rating'),
-    Field('people_rating'),
-    Field('atmosphere_rating'),
-    Field('cry_rating'),
-    Field('additional_comments'),
-    Field('images'),
-    Field('helpful_count'),
+    Field('noise_rating', 'integer'),
+    Field('people_rating', 'integer'),
+    Field('atmosphere_rating', 'integer'),
+    Field('cry_rating', 'integer'),
+    Field('additional_comments', 'text'),
+    Field('images', Field('image', 'upload', default='path/to/file')), #******
+    Field('helpful_count', 'integer'),
 )
 
 db.define_table(
     'student_reviews',
-    Field('date_review_posted', 'reference user_profiles', 'reference weep_reviews', ondelete="CASCADE"),
-    Field('number_of_reviews'),
-
+    Field('date_review_posted', 'reference user_profiles', 'reference weep_reviews', 'datetime', ondelete="CASCADE"),
+    Field('number_of_reviews', 'integer'),
 )
 
 db.define_table(
     'user_profiles',
-    Field('Name'),
-    Field('Email'),
-    Field('user_id'),
+    Field('Name', 'text', ondelete = "CASCADE"),
+    Field('Email', 'text', default=get_user_email),
+    Field('user_id', 'text'),
 )
+db.user_profiles.Name.requires = IS_NOT_EMPTY()
+db.user_profiles.Email.requires = IS_NOT_EMPTY()
+db.user_profiles.user_id.requires = IS_NOT_EMPTY()
+db.user_profiles.Name.readable = db.user_profiles.Name.writable = False
+db.user_profiles.Email.readable = db.user_profiles.Email.writable = False
+db.user_profiles.Email.readable = db.user_profiles.Email.writable = True
 
 
 db.commit()
